83b7706db5d851b3d76627f587b90cf61b4052b5
markoclimako <mkclimako@gmail.com>
2023-05-31 15:29:09 -0300

    req #10

--- src/components/Missions/index.tsx
+++ src/components/Missions/index.tsx
@@ -1,9 +1,22 @@
 import Title from '../Title';
+import missions from '../../data/missions';
+import MissionCard from '../MissionCard';
 
 export default function Missions() {
   return (
     <div data-testid="missions">
       <Title headline="Missões" />
+      {missions.map((mission) => {
+        return (
+          <MissionCard
+            name={ mission.name }
+            country={ mission.country }
+            year={ mission.year }
+            destination={ mission.destination }
+            key={ mission.name }
+          />
+        );
+      })}
     </div>
   );
 }
