c4040f9f43216cf33dd60b0e8e1b36321af554d6
markoclimako <mkclimako@gmail.com>
2023-05-31 00:21:22 -0300

    req #5

---
+++ src/components/PlanetCard/index.tsx
@@ -0,0 +1,15 @@
+import './index.css';
+
+interface PlanetCardProps {
+  planetName: string;
+  planetImage: string;
+}
+
+export default function PlanetCard({ planetName, planetImage } : PlanetCardProps) {
+  return (
+    <div className="planet-card-container" data-testid="planet-card">
+      <p data-testid="planet-name">{ planetName }</p>
+      <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+    </div>
+  );
+}
