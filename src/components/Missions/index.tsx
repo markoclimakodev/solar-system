1d6222e3c886352c0eaef212684942c525c93e16
markoclimako <mkclimako@gmail.com>
2023-05-31 15:16:44 -0300

    req #8

--- src/components/Missions/index.tsx
+++ src/components/Missions/index.tsx
@@ -1,3 +1,9 @@
+import Title from '../Title';
+
 export default function Missions() {
-  return <div data-testid="missions" />;
+  return (
+    <div data-testid="missions">
+      <Title headline="Missions" />
+    </div>
+  );
 }
